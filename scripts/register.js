9939d79a6c7e4489ac8f61f4c610be537b691e27
Valentina valencia <valentinavalencia@iMac-de-Valentina.local>
2020-11-13 00:44:38 -0500

    ya se puede hacer el registro y se subio a la base de datos

--- scripts/register.js
+++ scripts/register.js
@@ -1,6 +1,18 @@
-firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
-  });
+const register = document.querySelector('.register');
+
+register.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    const email = register.email.value;
+    const password = register.password.value;
+
+
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+      });
+
+});
+
