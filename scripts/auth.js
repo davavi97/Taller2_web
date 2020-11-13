d06923899b26814aa54ef9a8822cca5b2df5ad6b
Valentina valencia <valentinavalencia@iMac-de-Valentina.local>
2020-11-13 09:01:52 -0500

    Hay un problema en el auth.js

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,4 +1,39 @@
+const auth=document.querySelector('.auth');
+const authWhit=auth.querySelector('.auth__with');
+const authWhitout=auth.querySelector('.auth__without');
+const authProfileSpan=auth.querySelector('.auth__profile span');
+
+//console.log(auth, authWhit, authWhitout);
+
+
+
 firebase.auth().onAuthStateChanged(function(user) {
+    console.log(user);
+    //si el usuario existe quiere decir que inició sesión o se registró
+    if(user){
+        authWhit.classList.remove('hidden');
+        authWhitout.classList.add('hideen');
+
+
+
+        const db = firebase.firestore();
+        const usersRef = db.collection('users'); 
+        usersRef.doc(user.uid).get().then(function(doc){
+            if(doc.exists){
+                const data=doc.data();
+                authProfileSpan.innerText =data.firstname;
+
+            }
+            
+        });
+
+    } else {
+    //si no existe quiere decir que inició sesión o se registró
+    authWhit.classList.add('hidden');
+    authWhitout.classList.remove('hideen');
+
+    }
+    /*
     if (user) {
       // User is signed in.
       var displayName = user.displayName;
@@ -13,4 +48,5 @@ firebase.auth().onAuthStateChanged(function(user) {
       // User is signed out.
       // ...
     }
+    */
   });
