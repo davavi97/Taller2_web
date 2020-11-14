e2cdadcfd7686ff5a9ec953344e92d438f67ee5a
Valentina valencia <valentinavalencia@iMac-de-Valentina.local>
2020-11-14 17:52:28 -0500

    esta hecho el login

---
+++ scripts/login.js
@@ -0,0 +1,38 @@
+/*
+const db = firebase.firestore();
+const usersRef = db.collection('users');
+*/
+
+
+const login = document.querySelector('.login');
+
+login.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    const email = login.email.value;
+    const password = login.password.value;
+    
+
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(function(credentials){
+
+       
+            window.location.href='index.html'
+
+    
+
+        
+    })
+    
+    
+    .catch(function(error) {
+        // Handle Errors here.
+        console.log(error);
+
+        login.querySelector('.form__error').classList.remove('hidden');
+        // ...
+      });
+
+});
+
